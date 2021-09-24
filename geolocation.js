aa32f1bd8db6829f2cfb7edc9968ef0ed097cca7
Vakishna Thayalan <19279795@students.ltu.edu.au>
2021-09-25 04:56:16 +1000

    added html elements, js files, listen for lat and long input change

--- geolocation.js
+++ geolocation.js
@@ -1,1 +1,11 @@
-﻿
+﻿/* 
+us1.locationiq.com/v1/reverse.php?key=pk.a640e0113a33941722dfa49eed766c02&lat=-37.9060602&lon=145.1941778&format=json
+*/
+
+$('#latitude').on('input', function () {
+    console.log("Lat: " + $(this).val());
+});
+
+$('#longitude').on('input', function () {
+    console.log("Long: " + $(this).val());
+});
