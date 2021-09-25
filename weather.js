9a19d86f9bc18a21d1acad760d97b83964536444
Vakishna Thayalan <19279795@students.ltu.edu.au>
2021-09-25 12:53:54 +1000

    Fill Table Partially Done

--- weather.js
+++ weather.js
@@ -1,7 +1,38 @@
 ﻿function fillTable(respArr) {
     console.log(respArr);
     console.log(respArr[0].dt_txt);
-    var year = 
+
+    var current = 1;
+
+    for (var i = 0; i < respArr.length; i++) {
+        var dateTxt = new Date(respArr[i].dt_txt);
+
+        if (dateTxt.getHours() == 0 || i == 0) {
+            $("#d" + current + "day").text(dateTxt.toDateString());
+            $("#d" + current + "0").text(respArr[i].main.temp);
+        }
+
+        if (dateTxt.getHours() == 3) {
+            $("#d" + current + "3").text(respArr[i].main.temp);
+        }
+
+        if (dateTxt.getHours() == 21) {
+            $("#d" + current + "21").text(respArr[i].main.temp);
+            current += 1;
+        }
+    }
+
+
+
+
+
+
+
+
+    
+    
+    console.log(theDate.getDate());
+    console.log(weekday[theDate.getDay()]);
 
     //var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
 
