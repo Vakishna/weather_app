8c8e476cf5c268f07e809a467e1010468a864b10
Vakishna <vakishna1@gmail.com>
2021-09-25 16:06:30 +1000

    completed weather table

--- weather.js
+++ weather.js
@@ -7,7 +7,11 @@
     for (var i = 0; i < respArr.length; i++) {
         var dateTxt = new Date(respArr[i].dt_txt);
 
-        if (dateTxt.getHours() == 0 || i == 0) {
+        if (i == 0) {
+            $("#d" + current + "day").text(dateTxt.toDateString());
+        }
+
+        if (dateTxt.getHours() == 0) {
             $("#d" + current + "day").text(dateTxt.toDateString());
             $("#d" + current + "0").text(respArr[i].main.temp);
         }
@@ -16,33 +20,31 @@
             $("#d" + current + "3").text(respArr[i].main.temp);
         }
 
+        if (dateTxt.getHours() == 6) {
+            $("#d" + current + "6").text(respArr[i].main.temp);
+        }
+
+        if (dateTxt.getHours() == 9) {
+            $("#d" + current + "9").text(respArr[i].main.temp);
+        }
+
+        if (dateTxt.getHours() == 12) {
+            $("#d" + current + "12").text(respArr[i].main.temp);
+        }
+
+        if (dateTxt.getHours() == 15) {
+            $("#d" + current + "15").text(respArr[i].main.temp);
+        }
+
+        if (dateTxt.getHours() == 18) {
+            $("#d" + current + "18").text(respArr[i].main.temp);
+        }
+
         if (dateTxt.getHours() == 21) {
             $("#d" + current + "21").text(respArr[i].main.temp);
             current += 1;
         }
     }
-
-
-
-
-
-
-
-
-    
-    
-    console.log(theDate.getDate());
-    console.log(weekday[theDate.getDay()]);
-
-    //var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
-
-
-    //$("#forcastTable").append("<tr><td>Lol</td></tr > ")
-
-
-
-
-
 }
 
 $('#btnGetWeather').click(function () {
@@ -55,9 +57,9 @@ $('#btnGetWeather').click(function () {
     }).done(function (response) {
         var res = JSON.parse(response);
         fillTable(res.list);
-
     }).fail(function (response) {
         console.log("Weather Error" + response);
+        alert("Error filling table!");
     });
    alert(uriWeather);
 });
