586914d3d1c61b03e0487f296ff23431348a9a03
Vakishna Thayalan <19279795@students.ltu.edu.au>
2021-09-29 16:33:53 +1000

    Chart Done!

--- weather.js
+++ weather.js
@@ -1,4 +1,101 @@
-﻿function fillTable(respArr) {
+﻿function plotChart(respArr) {
+
+    var chart = document.getElementById("myCanvas");
+    var ctx = chart.getContext("2d");
+
+    // One day = 160 (space of x) 
+    // 8 time points a day (160 / 8 = 20)
+    ctx.moveTo(160, 0);
+    ctx.lineTo(160, 625);
+    ctx.stroke();
+
+    ctx.moveTo(320, 0);
+    ctx.lineTo(320, 625);
+    ctx.stroke();
+
+    ctx.moveTo(480, 0);
+    ctx.lineTo(480, 625);
+    ctx.stroke();
+
+    ctx.moveTo(640, 0);
+    ctx.lineTo(640, 625);
+    ctx.stroke();
+
+    var current = 1;
+
+    for (var i = 0; i < respArr.length; i++) {
+
+        var dateTxt = new Date(respArr[i].dt_txt);
+        var plotSegment = (current * 160) - 160;
+             
+        if (i == 0) {
+            dateTxt.toDateString();
+            ctx.fillText(dateTxt.toDateString(), 5, 30);
+            ctx.beginPath();
+        }
+
+        if (dateTxt.getHours() == 0) {
+            var plotX = plotSegment + 0;
+            var plotY = 625 - (respArr[i].main.temp * 12.5)        
+            ctx.fillText(dateTxt.toDateString(), plotSegment + 5, 30);
+            ctx.arc(plotX, plotY, 5, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+        if (dateTxt.getHours() == 3) {
+            var plotX = plotSegment + 20;
+            var plotY = 625 - (respArr[i].main.temp * 12.5);
+            ctx.arc(plotX, plotY, 5, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+        if (dateTxt.getHours() == 6) {
+            var plotX = plotSegment + 40;
+            var plotY = 625 - (respArr[i].main.temp * 12.5);
+            ctx.arc(plotX, plotY, 5, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+        if (dateTxt.getHours() == 9) {
+            var plotX = plotSegment + 60;
+            var plotY = 625 - (respArr[i].main.temp * 12.5);
+            ctx.arc(plotX, plotY, 5, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+        if (dateTxt.getHours() == 12) {
+            var plotX = plotSegment + 80;
+            var plotY = 625 - (respArr[i].main.temp * 12.5);
+            ctx.arc(plotX, plotY, 5, 0, 2 * Math.PI);
+            ctx.stroke();
+
+        }
+
+        if (dateTxt.getHours() == 15) {
+            var plotX = plotSegment + 100;
+            var plotY = 625 - (respArr[i].main.temp * 12.5);
+            ctx.arc(plotX, plotY, 5, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+        if (dateTxt.getHours() == 18) {
+            var plotX = plotSegment + 120;
+            var plotY = 625 - (respArr[i].main.temp * 12.5);
+            ctx.arc(plotX, plotY, 5, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+        if (dateTxt.getHours() == 21) {
+            var plotX = plotSegment + 140;
+            var plotY = 625 - (respArr[i].main.temp * 12.5);
+            ctx.arc(plotX, plotY, 5, 0, 2 * Math.PI);
+            ctx.stroke();   
+            current++;
+        }
+    }
+}
+
+function fillTable(respArr) {
     console.log(respArr);
     console.log(respArr[0].dt_txt);
 
@@ -57,6 +154,7 @@ $('#btnGetWeather').click(function () {
     }).done(function (response) {
         var res = JSON.parse(response);
         fillTable(res.list);
+        plotChart(res.list);
     }).fail(function (response) {
         console.log("Weather Error" + response);
         alert("Error filling table!");
